1d56204ee3e1965438aa339c9da230bcd474e1a3
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-05-02 15:42:11 +0300

    parser is done

--- scripts/parser.js
+++ scripts/parser.js
@@ -11,6 +11,13 @@ function parse(s)
 		case "date": {
 			return Date();
 		}
+		case "./keygen": {
+			if (window.path != "/bin/") {
+				return "bash: cd: " + s[1] + ": No such file or directory";
+			}
+		}
+		case "../bin/keygen":
+		case "/bin/keygen":
 		case "keygen": {
 			return "donotusethiskeypls";
 		}
@@ -31,10 +38,84 @@ function parse(s)
 			return ans;
 		}
 		case "cat": {
-
+			switch (window.path) {
+				case "/bin/": {
+					switch (s[1]) {
+						case "keygen":
+							return "#!/bin/bash<br />echo donotusethiskeypls";
+						case "exitpls":
+							return "#!/bin/bash<br />" +
+								   "if [ $1 = 'donotusethiskeypls' ] then" +
+								   "<br />exit<br />fi";
+						default:
+							return "cat: " + s[1] + ": No such file or directory";
+					}
+				}
+				case "/Dummy/": {
+					switch (s[1]) {
+						case "Diary1": {
+							return "I am so aloneeee!!! Ugh! I wish that " +
+							       "someone logs in to my computer so that " +
+							       "I can talk with them.";
+						}
+						case "Diary2": {
+							return "Well... If someone falls in to my " +
+							       "computer somehow, they are not going to " +
+							       "get out that easily. Because I modified " +
+							       "my bash so that they can't escape. " +
+							       "HAHAHA! They have to find my special " +
+							       "script to get out. But they will never" +
+							       "find it and we will talk forever. HAHAHA!";
+						}
+						case "password": {
+							return "My locker password is: iamthebest. " +
+							       "It's really secure isn't it?"
+						}
+						default: {
+							return "cat: " + s[1] + ": No such file or directory";
+						}
+					}
+				}
+				case "/xxxSecretxxx/": {
+					switch (s[1]) {
+						case "secretpass": {
+							return "extremelysecretpass";
+						}
+						case "my_favorite_song": {
+							return "I Love My Friends";
+						}
+						case "my_secret": {
+							return "OMG I LOVE ACTING! I will be an actor " +
+							       "when I have so many friends!";
+						}
+					}
+				}
+			}
 		}
+		case "./exitpls": {
+			if (window.path != "/bin/") {
+				return "bash: cd: " + s[1] + ": No such file or directory";
+			}
+		}
+		case "../bin/exitpls":
+		case "/bin/exitpls":
 		case "exitpls": {
-
+			if (s[1] == "donotusethiskeypls") {
+				return "Well done!";
+			}
+			return "";
+		}
+		case "ls": {
+			switch (window.path) {
+				case "/":
+					return "bin Dummy xxxSecretxxx";
+				case "/bin/":
+					return "keygen exitpls";
+				case "/Dummy/":
+					return "Diary1 Diary2 password";
+				case "/xxxSecretxxx/":
+					return "secretpass my_favorite_song my_secret";
+			}
 		}
 		case "cd": {
 			switch (s[1]) {
@@ -51,14 +132,39 @@ function parse(s)
 				case "/xxxSecretxxx":
 				case "/xxxSecretxxx/": {
 					window.path = "/xxxSecretxxx/";
+					return "";
 				}
-				case "bin":
-				case "Dummy":
-				case "xxxSecretxx":
-				case "..":
+				case "/": {
+					window.path = "/";
+					return "";
+				}
+				case "bin": {
+					if (window.path == "/") {
+						window.path = "/bin/";
+						return "";
+					}
+					return "bash: cd: " + s[1] + ": No such file or directory";
+				}
+				case "Dummy": {
+					if (window.path == "/") {
+						window.path = "/Dummy/";
+						return "";
+					}
+					return "bash: cd: " + s[1] + ": No such file or directory";
+				}
+				case "xxxSecretxxx": {
+					if (window.path == "/") {
+						window.path = "/xxxSecretxxx/";
+						return "";
+					}
+					return "bash: cd: " + s[1] + ": No such file or directory";
+				}
+				case "..": {
+					window.path = "/";
+					return "";
+				}
 				case ".": {
-					return "Relative file paths are disabled.";
-					// I am too lazy to implement that
+					return "";
 				}
 				default: {
 					return "bash: cd: " + s[1] + ": No such file or directory";
