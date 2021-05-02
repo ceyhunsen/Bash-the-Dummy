17f82ae169f55e9acc519305cddd72a524dd0619
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-05-02 18:59:14 +0300

    dummy actions added

--- scripts/dummy.js
+++ scripts/dummy.js
@@ -1,7 +1,42 @@
 function dummy(command)
 {
+	/*
+	 * 0:  help
+	 * 1:  date
+	 * 2:  run keygen
+	 * 3:  pwd
+	 * 4:  echo
+	 * 5:  echo to file
+	 * 6:  cat wrong file
+	 * 7:  cat keygen
+	 * 8:  cat exitpls
+	 * 9:  cat Diary1
+	 * 10: cat Diary2
+	 * 11: cat password
+	 * 12: cat secretpass
+	 * 13: cat my_favorite_song
+	 * 14: cat my_secret
+	 * 15: exitpls
+	 * 16: exitpls wrong password
+	 * 17: ls
+	 * 18: cd bin
+	 * 19: cd Dummy
+	 * 20: cd xxxSecretxxx
+	 * 21: cd /
+	 * 22: access denied
+	 * 23: wrong command
+	 * */
+	var image = document.getElementById("dummy_image");
+	var text = document.getElementById("dummy_speech");
 	switch(command) {
-		case 0:
-			document.getElementById("dummy_image").src="/images/dummy_crying.png";
+		case 0: {
+			image.src = "/images/dummy_crying.png";
+			text.innerText = "Nooo! Why do you need help? We are gonna have fun here. " +
+			"You don't need help or anyone other than me!";
+			break;
+		}
+		case 1: {
+			image.src = "";
+		}
 	}
 }
--- scripts/parser.js
+++ scripts/parser.js
@@ -10,26 +10,32 @@ function parse(s)
 			return "You called for help... But nobody came.";
 		}
 		case "date": {
+			dummy(1);
 			return Date();
 		}
 		case "./keygen": {
 			if (window.path != "/bin/") {
+				dummy(22);
 				return "bash: cd: " + s[1] + ": No such file or directory";
 			}
 		}
 		case "../bin/keygen":
 		case "/bin/keygen":
 		case "keygen": {
+			dummy(2);
 			return "donotusethiskeypls";
 		}
 		case "pwd": {
+			dummy(3);
 			return window.path;
 		}
 		case "echo": {
+			dummy(4);
 			if (s[1] == "$PATH") {
 				return "/bin";
 			}
 			for (var i = 1; i < s.length; i++) {
+				dummy(5);
 				if (s[i] == ">" || s[i] == ">>") {
 					return "bash: " + s[i + 1] + ": Permission denied";
 				} else {
@@ -42,24 +48,32 @@ function parse(s)
 			switch (window.path) {
 				case "/bin/": {
 					switch (s[1]) {
-						case "keygen":
+						case "keygen": {
+							dummy(7)
 							return "#!/bin/bash<br />echo donotusethiskeypls";
-						case "exitpls":
+						}
+						case "exitpls": {
+							dummy(8);
 							return "#!/bin/bash<br />" +
 								   "if [ $1 = 'donotusethiskeypls' ] then" +
 								   "<br />exit<br />fi";
-						default:
+						}
+						default: {
+							dummy(6);
 							return "cat: " + s[1] + ": No such file or directory";
+						}
 					}
 				}
 				case "/Dummy/": {
 					switch (s[1]) {
 						case "Diary1": {
+							dummy(9);
 							return "I am so aloneeee!!! Ugh! I wish that " +
 							       "someone logs in to my computer so that " +
 							       "I can talk with them.";
 						}
 						case "Diary2": {
+							dummy(10);
 							return "Well... If someone falls in to my " +
 							       "computer somehow, they are not going to " +
 							       "get out that easily. Because I modified " +
@@ -69,10 +83,12 @@ function parse(s)
 							       "find it and we will talk forever. HAHAHA!";
 						}
 						case "password": {
+							dummy(11);
 							return "My locker password is: iamthebest. " +
 							       "It's really secure isn't it?"
 						}
 						default: {
+							dummy(6);
 							return "cat: " + s[1] + ": No such file or directory";
 						}
 					}
@@ -80,14 +96,21 @@ function parse(s)
 				case "/xxxSecretxxx/": {
 					switch (s[1]) {
 						case "secretpass": {
+							dummy(12);
 							return "extremelysecretpass";
 						}
 						case "my_favorite_song": {
+							dummy(13);
 							return "I Love My Friends";
 						}
 						case "my_secret": {
+							dummy(14);
 							return "OMG I LOVE ACTING! I will be an actor " +
 							       "when I have so many friends!";
+						}
+						default: {
+							dummy(6);
+							return "cat: " + s[1] + ": No such file or directory";
 						}
 					}
 				}
@@ -95,6 +118,7 @@ function parse(s)
 		}
 		case "./exitpls": {
 			if (window.path != "/bin/") {
+				dummy(22);
 				return "bash: cd: " + s[1] + ": No such file or directory";
 			}
 		}
@@ -102,11 +126,15 @@ function parse(s)
 		case "/bin/exitpls":
 		case "exitpls": {
 			if (s[1] == "donotusethiskeypls") {
-				return "Well done!";
+				dummy(15);
+				return "";
+			} else {
+				dummy(16);
 			}
 			return "";
 		}
 		case "ls": {
+			dummy(17);
 			switch (window.path) {
 				case "/":
 					return "bin Dummy xxxSecretxxx";
@@ -120,47 +148,42 @@ function parse(s)
 		}
 		case "cd": {
 			switch (s[1]) {
+				case "bin": {
+					if (window.path != "/") {
+						return "bash: cd: " + s[1] + ": No such file or directory";
+					}
+				}
 				case "/bin/":
 				case "/bin": {
+					dummy(18);
 					window.path = "/bin/";
 					return "";
 				}
+				case "Dummy": {
+					if (window.path != "/") {
+						return "bash: cd: " + s[1] + ": No such file or directory";
+					}
+				}
 				case "/Dummy":
 				case "/Dummy/": {
+					dummy(19);
 					window.path = "/Dummy/";
 					return "";
 				}
+				case "xxxSecretxxx": {
+					if (window.path != "/") {
+						return "bash: cd: " + s[1] + ": No such file or directory";
+					}
+				}
 				case "/xxxSecretxxx":
 				case "/xxxSecretxxx/": {
+					dummy(20);
 					window.path = "/xxxSecretxxx/";
 					return "";
 				}
-				case "/": {
-					window.path = "/";
-					return "";
-				}
-				case "bin": {
-					if (window.path == "/") {
-						window.path = "/bin/";
-						return "";
-					}
-					return "bash: cd: " + s[1] + ": No such file or directory";
-				}
-				case "Dummy": {
-					if (window.path == "/") {
-						window.path = "/Dummy/";
-						return "";
-					}
-					return "bash: cd: " + s[1] + ": No such file or directory";
-				}
-				case "xxxSecretxxx": {
-					if (window.path == "/") {
-						window.path = "/xxxSecretxxx/";
-						return "";
-					}
-					return "bash: cd: " + s[1] + ": No such file or directory";
-				}
+				case "/":
 				case "..": {
+					dummy(21);
 					window.path = "/";
 					return "";
 				}
@@ -176,9 +199,10 @@ function parse(s)
 		case "sudo":
 		case "mkdir":
 		case "touch": {
+			dummy(22);
 			return "bash: " + s[0] + ": Permission denied";
 		}
 	}
-
+	dummy(23);
 	return "bash: " + s[0] + ": command not found...";
 }
